922ab8f26b854ed96de9a81527ec935e9403479e
552020 <stefanolombardo@posteo.de>
2024-10-18 18:19:05 +0200

    Chore: fix setup for development'

--- src/backend/websocket_demo/settings.py
+++ src/backend/websocket_demo/settings.py
@@ -27,16 +27,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+# Print the SECRET_KEY for debugging (remove this in production)
+print("SECRET_KEY:", SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if ENVIRONMENT == "production":
     DEBUG = False
-    ALLOWED_HOSTS = ["*"]
-else:
-    DEBUG = True
     ALLOWED_HOSTS = (
         os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
     )
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = ["*"]
+    # ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
