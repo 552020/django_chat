b1dbfdbfad96428f03b240cb269d81fd99d493b3
552020 <stefanolombardo@posteo.de>
2024-10-18 19:12:20 +0200

    Chore: tweak STATICFILES_DIRS

--- src/backend/websocket_demo/settings.py
+++ src/backend/websocket_demo/settings.py
@@ -148,7 +148,10 @@ else:
     STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "src", "backend", "chat", "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
